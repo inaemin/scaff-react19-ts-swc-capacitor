f31623c1f2eed60b42a6edd8f7aa4ebdacb6a1d7
aaaz425 <aaaz425@naver.com>
2025-01-24 17:28:14 +0900

    style: install prettier and apply it

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'scaff-react19-ts-swc-capacitor',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'http',
-    cleartext: true
-  },
+    appId: 'com.example.app',
+    appName: 'scaff-react19-ts-swc-capacitor',
+    webDir: 'dist',
+    server: {
+        androidScheme: 'http',
+        cleartext: true,
+    },
 };
 
 export default config;
