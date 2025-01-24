10ca396e591b9b3a04dc111e31f687e52e84238f
민인애 <inaemin@gmail.com>
2025-01-24 17:02:25 +0900

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'scaff-react19-ts-swc-capacitor',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    cleartext: true
+  },
 };
 
 export default config;
